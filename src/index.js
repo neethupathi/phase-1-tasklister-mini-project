9cf50cd3580590b24b056f921e05eaa9b251e2a3
neethupath <pathnee222gmail.com>
2023-04-14 16:52:17 -0400

    form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const t=document.getElementById("tasks");
+  document.getElementById("create-task-form").addEventListener("submit",
+  function but(e)
+  {
+    e.preventDefault();
+    let txt = document.getElementById("new-task-description").value;
+   const li = document.createElement('li');
+    li.appendChild(document.createTextNode(txt));
+    const delBtn = document.createElement('button');
+    delBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(delBtn);
+t.appendChild(li);
+  }
+  );
+  t.addEventListener('click',
+  function rem(e) {
+    if (e.target.nodeName === 'BUTTON') {
+      if(confirm('Are you sure?')) {
+        let li = e.target.parentElement;
+        t.removeChild(li);
+      }
+    }
+
+  }
+  );
+
+
 });
+
+
